9e03f590e5f92705ab8ec12fffdbde3d87c6bb9e
KanashiXI <panyapat.test@gmail.com>
2021-01-07 14:29:41 +0700

    back 3rd

---
+++ TCRUWUback/app/Http/Controllers/EditprofileController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+header('Access-Control-Allow-Origin : *');
+header('Access-Control-Allow-Methods : *');
+header('Access-Control-Allow-Headers : Content-type, X-Auth-Token, Authorization, Origin');
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\users;
+use App\User;
+
+class EditprofileController extends Controller
+{
+    public function editprofile(Request $request, User $user)
+    {
+        $edit=User::where('email', $request->email)->where('email',$request->email)->first();
+        $edit->email=$request->email;
+        $edit->name_title=$request->name_title;
+        $edit->firstname=$request->firstname;
+        $edit->lastname=$request->lastname;
+        $edit->address=$request->address;
+        $edit->postal_code=$request->postal_code;
+        $edit->province=$request->province;
+        $edit->district=$request->district;
+        $edit->telephone=$request->telephone;
+        $edit->sub_district=$request->sub_district;       
+        $result = $edit->save();
+    }
+}
---
+++ TCRUWUback/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\customer;
+use App\users;
+
+class RegisterController extends Controller
+{
+    
+    public function getcustomer()
+    {
+        // if(isset($request -> id)){
+        //     $getCustomerByUsername = customer::find($request -> id);
+        // return response()->json($getCustomerByUsername,201); 
+        // }
+        $getall = customer::all();
+        return response()->json($getall,200); 
+    }
+
+
+    
+    public function getCustomerByUsername($request)
+    {   
+        // $id = intval($request);
+        // $data = $request->get('username');
+        // $data = $request->get('username');
+
+        $getUsernameCustomer = customer::where('username', $request)->get();      
+        return response()->json($getUsernameCustomer,200);        
+    }
+
+    //เช็คเมลซ้ำใช้อันนี้
+    public function getCustomerByEmail($request)
+    { 
+        $getEmailCustomer = users::where('email', $request)->get();      
+        return response()->json($getEmailCustomer,200); 
+    }
+
+
+
+    public function createcustomer(Request $request)
+    {
+        $new = new customer;
+        $new->username = $request->input('username');
+        $new->password = $request->input('password');
+        $new->firstname = $request->input('firstname');
+        $new->lastname = $request->input('lastname');
+        // $new->mail = $request->input('mail');
+        $new->telephone = $request->input('telephone');
+        $new->role_id = $request->input('role_id');
+        
+        $new->save();
+
+        return response()->json(['newitem'=>$new],201);
+    }
+    
+}
